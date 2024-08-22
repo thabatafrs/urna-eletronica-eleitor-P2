81ab403d9c2f5022776cd6d70a800acb354cbb07
thabatafrs <thabatafreitas10@gmail.com>
2024-08-22 12:43:44 -0300

    first commit

---
+++ docs/mainEleitor.js
@@ -0,0 +1,9 @@
+function cancelar() {
+  document.getElementById("cancela");
+  location.href = "voto.html";
+}
+
+function confirma() {
+  document.getElementById("confirma");
+  location.href = "FIM.html";
+}
